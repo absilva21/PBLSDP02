c5ac4cd00f763445eedad727667727e8f9efe5af
absilva21 <83670712+absilva21@users.noreply.github.com>
2022-11-18 11:59:07 -0300

    Add files via upload

--- PBLSD.ino
+++ PBLSD.ino
@@ -1,26 +1,30 @@
-#include "gpio.h" 
-#include "uart.h"
+
+const int ledPin = LED_BUILTIN; 
 int analogValue;
+int digitalValue;
 int comand;
 int addr;
+
 void setup() {
-  // put your setup code here, to run once:
-   GPIO_OUTPUT_SET(GPIO_ID_PIN(2),1);
-   Serial.begin(9600);
+  //Desligando a led 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin,HIGH);
+  //Iniciando a UART e variaveis 
+   Serial1.begin(9600);
    analogValue = 0;
    comand = 0;
    addr = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()  == 2){
-    comand = Serial.read();
-    addr = Serial.read();
+  //Caso a Uart esteja com dois bytes na fila
+  if(Serial1.available()  == 2){
+    comand = Serial1.read();
+    addr = Serial1.read();
     interp(comand,addr);
   }
 }
-
+//Interpreta o comando recebido pela UART
 void interp(int com, int adds){
   switch (com){
     case 0x03:
@@ -30,21 +34,31 @@ void interp(int com, int adds){
       inputAnalog(adds);
     break;
     case 0x05:
-    break:
+      inputDigital(adds);
+    break;
+    case 0x06:
+      digitalWrite(ledPin,LOW);
+    break;
   }
 }
-
+//Envia o status do dispositivo
 void status(){
   if(ESP.checkFlashCRC()){
-    Serial.print(0x00,HEX);
+    Serial1.print(0x00,HEX);
   }else{
-    Serial.print(0x1F,HEX);
+    Serial1.print(0x1F,HEX);
   }
 }
-
+//Envia o valor da entrada analógica
 void inputAnalog(int adds){
   analogValue = analogRead(A0);
-  Serial.print(0x01,HEX);
-  Serial.print(analogValue);
+  Serial1.print(0x01,HEX);
+  Serial1.print(analogValue,HEX);
   analogValue = 0;
+}
+//Envia o valor da entrada digital
+void inputDigital(int adds){
+  digitalValue = digitalRead(16);
+  Serial1.print(0x02,HEX);
+  Serial1.print(digitalValue,HEX);
 }
