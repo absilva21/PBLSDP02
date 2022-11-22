9c9238be7686ba662c26d06683e5b70c5138dc14
absilva21 <83670712+absilva21@users.noreply.github.com>
2022-11-22 20:43:55 -0300

    Add files via upload

--- sbc.c
+++ sbc.c
@@ -3,11 +3,18 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-
-const char inicio[12] = "iniciando..."
-const char erroini[21] = "erro na inicianizacao"
-int analogValue;
-int digitalValue;
+#include <stdio.h>
+const char led[2] = {0x06,0x06};
+const char status[2] = {0x03,0x03};
+const char anologCmd[2] = {0x04,0x04};
+const char digitalCmd[2] = {0x05,0x05};
+unsigned char analogBuffer[2];
+unsigned char digitalBuffer[2];
+const char inicio[12] = "iniciando...";
+const char erroini[21] = "erro na inicianizacao";
+const char strAnalogValue[26] = "Valor do sensor analogico ";
+char analogValue[3];
+cha digitalValue[3];
 char string[30];
 char *portname = "/dev/ttys0"
 
@@ -39,8 +46,26 @@ void main(){
     tty.c_cc[VTIME] = 5;    
     tty.c_iflag &= ~(IXON | IXOFF | IXANY);
     tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~(PARENB | PARODD);  
+    tty.c_cflag |= parity;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+    if(tcsetattr (fd, TCSANOW, &tty) != 0)
+    {
+    	print(erroini,21);
+            
+    }
+    
+    set_blocking (fd, 0);   
     while(1){
-    	
+        write(fd,anologCmd,2);
+        usleep(10);
+        read(fd,analogBuffer,2);
+        print(strAnalogValue,26);
+        sprinf(analogValue,"%d",analogBuffer[1]);
+        print(analogValue,2);
+        clear();
+        usleep(3000);
     }
 }
 
@@ -48,4 +73,21 @@ void print(char string[],int size){
     for(i=0;i<size;i++){
         lcdWrite(string[size]);
     }
+}
+
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                error_message ("error %d from tggetattr", errno);
+                return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                error_message ("error %d setting term attributes", errno);
 }
